d9ea14d344e4376d3e3e11b402486ee118ef9d4a
Luca Borges <143541175+LucaBorgesLima@users.noreply.github.com>
2024-08-30 23:02:46 -0300

    Delete fdgfd.sql

--- fdgfd.sql
+++
@@ -1,8 +0,0 @@
-select * from HumanResources.Employee
-select * from HumanResources.vEmployeeDepartment
-
-select JobTitle , count(Gender) from HumanResources.Employee
-where Gender = 'M'
-group by JobTitle
-
-
